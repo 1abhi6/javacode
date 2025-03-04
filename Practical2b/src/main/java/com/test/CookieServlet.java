84ac5c62105bde681562f6570728534c3487f8d7
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2025-03-04 09:48:50 +0530

    Add files via upload

---
+++ Practical2b/src/main/java/com/test/CookieServlet.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author lenovo
+ */
+@WebServlet(name = "CookieServlet", urlPatterns = {"/CookieServlet"})
+public class CookieServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        /* TODO output your page here. You may use following sample code. */
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet CookieServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        
+        /* Persistance Cookie has age but Sessions do not have age,
+        they expire immediately, once the browser is closed */
+        
+        if (request.getCookies() == null) {
+            Cookie ck = new Cookie("Cookie", "1");
+            ck.setMaxAge(30);
+            response.addCookie(ck);
+            out.println("Count 1");
+        } else {
+            // Retrive the cookie
+            Cookie[] ck = request.getCookies();
+            int x = Integer.parseInt(ck[0].getValue());
+            x++;
+
+            // Send the Cookie again
+            ck[0].setValue("" + x);
+            ck[0].setMaxAge(30);
+            response.addCookie(ck[0]);
+            out.println("Count " + x);
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
